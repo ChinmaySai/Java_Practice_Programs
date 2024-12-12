5689c7b879f8317fa4b00652be601cd11421c7bb
Chinmay Sai <yanamalachinmaysai106@gmail.com>
2024-12-12 22:46:38 +0530

    Committing code of SwapNumbers

---
+++ JAVA_BASIC_PROGRAMS2/src/SwapNumbers.java
@@ -0,0 +1,16 @@
+
+public class SwapNumbers {
+
+	public static void main(String[] args) {
+
+		int a=4,b=5;
+		int temp=0;
+		System.out.println("Value of a and b before swapping a="+a+" b="+b);
+		temp=a;
+		a=b;
+		b=temp;
+		System.out.println("Value of a and b after swapping a="+a+" b="+b);
+		
+	}
+
+}
