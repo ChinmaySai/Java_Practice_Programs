2b671b310bc680c1946f8137b6f6f23a59ec3138
Chinmay Sai <yanamalachinmaysai106@gmail.com>
2024-12-15 23:21:36 +0530

    Prime Number code changes

---
+++ JAVA_BASIC_PROGRAMS2/src/PrimeNumber.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class PrimeNumber {
+	public static void main(String args[])
+	{
+		
+		Scanner sc=new Scanner(System.in);
+		boolean isPrime=true;
+		System.out.println("Enter the number to check if it is prime/not");
+		int num=sc.nextInt();
+		for(int i=2;i<=num/2;i++)
+		{
+			if(num%i==0)
+			{
+				isPrime=false;
+				break;
+			}
+				
+		}
+		if(isPrime)
+		{
+			System.out.println(num+" is  Prime number");
+
+		}
+		else
+		{
+			System.out.println(num+" is not Prime number");
+		}
+			
+		
+	}
+
+}
