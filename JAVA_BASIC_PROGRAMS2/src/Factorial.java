39036e2c9b884df75740082e67bcc3164d87014a
Chinmay Sai <yanamalachinmaysai106@gmail.com>
2024-12-16 21:02:13 +0530

    Committing Factorial of number changes

---
+++ JAVA_BASIC_PROGRAMS2/src/Factorial.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/**
+ * Java Program to find the Factorial of a number
+ */
+public class Factorial {
+
+	public static void main(String[] args) {
+
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter the number : ");
+        int num=sc.nextInt();
+        long fact=1;
+        for(int i=1;i<=num;i++)
+        {
+        	fact=fact*i;
+        }
+        System.out.println("Factorial of "+num+" is "+fact);
+	}
+
+}
