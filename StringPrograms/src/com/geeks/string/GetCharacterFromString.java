9f8b2b80030be62b91aadd974c844d8a4b4eece4
Chinmay Sai <yanamalachinmaysai106@gmail.com>
2025-01-05 20:37:44 +0530

    Character from String Code Changes Commit.

---
+++ StringPrograms/src/com/geeks/string/GetCharacterFromString.java
@@ -0,0 +1,35 @@
+package com.geeks.string;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class GetCharacterFromString {
+
+	public static void main(String[] args) {
+
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter the input String :");
+		try
+		{
+		String input=br.readLine();
+		char a[]=new char[input.length()];
+	     a=input.toCharArray();
+	     System.out.println("Enter the index:");
+	     int index=Integer.parseInt(br.readLine());
+	     if(a.length>index)
+	     {
+	    	 System.out.println("Character at index "+index+" is "+a[index]);
+	     }
+	     else
+	     {
+	    	 System.out.println("Provided index with in the provided String Range");
+	     }
+	     
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+
+}
