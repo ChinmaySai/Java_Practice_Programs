b0aa16d230d09bc1528a10b9cff9b453aae7483c
Chinmay Sai <yanamalachinmaysai106@gmail.com>
2025-01-11 22:19:49 +0530

    Static Flow Controller Code Changes Commit.

---
+++ JAVA_BASIC_PROGRAMS2/src/StaticFlowController.java
@@ -0,0 +1,23 @@
+public class StaticFlowController {
+
+	static int a;
+	static int b;
+	static
+	{
+		a=10;
+		b=20;
+		System.out.println("static block");
+		disp();
+
+	}
+	static void disp()
+	{
+		System.out.println("static method");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Inside the Main Method");
+
+	}
+
+}
