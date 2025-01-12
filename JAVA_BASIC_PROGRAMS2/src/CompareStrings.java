5d93a94e01f3dca113b61eebca43229831ce600d
Chinmay Sai <yanamalachinmaysai106@gmail.com>
2025-01-13 00:00:32 +0530

    Comparing String Changes Commit.

---
+++ JAVA_BASIC_PROGRAMS2/src/CompareStrings.java
@@ -0,0 +1,13 @@
+public class CompareStrings {
+  
+    public static void main(String[] args) {
+      
+        String s1 = "Hello";
+        String s2 = "Chinmay";
+        String s3 = "Hello";
+
+        // Comparing strings
+        System.out.println(s1.equals(s2)); 
+        System.out.println(s1.equals(s3));
+    }
+}
