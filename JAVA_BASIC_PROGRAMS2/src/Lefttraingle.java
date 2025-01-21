5f2b055a5d00e6850fcde92701cf8f8bdfa94e05
Chinmay Sai <yanamalachinmaysai106@gmail.com>
2025-01-21 23:39:12 +0530

    Left Triangle Star Pattern Changes Commit.

---
+++ JAVA_BASIC_PROGRAMS2/src/Lefttraingle.java
@@ -0,0 +1,31 @@
+
+
+/**
+ * 
+ * 
+ * Java Program to Print Left Triangle Star Pattern
+ */
+
+public class Lefttraingle {
+
+	public static void main(String[] args) {
+
+		        
+		 
+		int j;
+		        int n=10;
+               for (int i = 0; i < n; i++) {
+ 
+            for (j = 2 * (n - i); j >= 0; j--) {
+                System.out.print(" ");
+            }
+ 
+            for (j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+ 
+            System.out.println();
+        }
+	}
+
+}
